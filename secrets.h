0f3ecbdec194947980964bbcff3914d59115cdb2
Lecoeliaque <143458879+LeCoeliaque@users.noreply.github.com>
2026-03-08 22:12:36 -0500

    Create secrets.h

---
+++ secrets.h
@@ -0,0 +1,4 @@
+char* SSID = "Put wifi SSID here";
+char* PASSWORD = "Put wifi password here";
+const char* CLIENT_ID = "Copy Client ID from app setup in spotify dev dashboard"
+const char* CLIENT_SECRET = "Copy Client Secret from app setup in spotify dev dashboard"
